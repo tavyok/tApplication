90e27a5fc8ec4978baaaba84366524094b46ba32
Sergiu Neamt <sergiu.neamt@nordlogic.com>
2013-11-23 17:08:06 +0200

    Initial

---
+++ application/controllers/IndexController.php
@@ -0,0 +1,29 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function indexAction()
+    {
+        // action body
+
+        $this->view->assign("name","index tavi");
+    }
+
+    public function mainAction()
+    {
+        // action body
+
+        $this->view->assign("name","tavi");
+    }
+
+    public function menuAction()
+    {
+        // action body
+
+        $this->view->assign("name","menu index tavi");
+    }
+
+
+}
+
---
+++ application/controllers/UserController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NicolaeSergiu
+ * Date: 11/23/13
+ * Time: 3:07 PM
+ */
+
+class UserController extends Zend_Controller_Action {
+
+
+    public function init(){
+        $this->_helper->_layout->setLayout('layout-page');
+    }
+
+    public function indexAction(){
+    }
+
+
+
+    public function addAction(){
+
+
+        if( $this->getRequest()->isPost()){
+
+            $params = $this->getRequest()->getParams();
+
+            Zend_Debug::dump($params["email"]);
+            Zend_Debug::dump($params["password"]);
+
+            $rememberMe = $this->getRequest()->getParam("rememberMe",0);
+            Zend_Debug::dump($rememberMe);
+
+            // adaugare
+
+            $this->redirect("/user");
+        }
+    }
+} 
